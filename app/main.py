ed7df60e2b54aa7bc5588abc581794d98e53d06a
aniketkumarsingh-9 <146952321+aniketkumarsingh-9@users.noreply.github.com>
2026-01-19 10:32:56 +0530

    Deploy Mini RAG on Hugging Face Spaces

--- app/main.py
+++ app/main.py
@@ -91,3 +91,13 @@ def query_documents(q : QueryInput):
             for r in search_results
         ]
     } 
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
